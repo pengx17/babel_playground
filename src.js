85ec675fe0ed629fff12445ac6c0ccaef199f209
Peng Xiao <pxiao@marinsoftware.com>
2016-01-12 13:40:30 +0800

    Add a parallel example.

--- src.js
+++ src.js
@@ -1,6 +1,23 @@
 // Write ES6 codes here
 
-const foo = async () => {
+// Return a new promise which will resolve itself after some milliseconds
+function delay(milliseconds) {
+  return new Promise(res => {
+    setTimeout(() => {
+      console.log(`Res after ${milliseconds} milliseconds`);
+      res();
+    }, milliseconds);
+  });
 }
-console.log(foo);    // foo is a async function
-console.log(foo());  // foo() is a promise
+
+// Run multiple promises in parallel
+const parallel = async () => {
+  const delays = [];
+  for (let i = 0; i < 10; i ++) {
+    delays.push(delay(1000 * i));
+  }
+  await Promise.all(delays);
+  console.log("parallel done");
+}
+
+parallel();
